8e16f01c17713c3feab742e26b5efac5a1fbe8b9
Alison-Azman <aazman@student.touro.edu>
2026-02-01 18:32:24 -0500

    Add files via upload

---
+++ About_Me_Github.py
@@ -0,0 +1,12 @@
+
+
+#Hi my name is Alison Azman
+#About me
+
+live="Baltimore"
+degree="Computer Science"
+favorite_color= "Pink"
+nickname="Reizi"
+job="I work in a school that mainly caters to children with special needs,\nand I do the insurance authorizations for them."
+
+print(f"Hi, my name is {nickname}! I live in {live} and I am going for {degree}.\nDuring the day, from 9-4, {job} \nI am excited to learn more coding skills.")
